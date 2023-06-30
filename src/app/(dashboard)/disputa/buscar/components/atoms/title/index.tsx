f475afa0497b747ea03ea23d7057a1545b129a08
aldcejam <aldcejamj923@gmail.com>
2023-06-30 15:13:03 -0300

    UPDATE STYLE: title page disputa/buscar

--- src/app/(dashboard)/disputa/buscar/components/atoms/title/index.tsx
+++ src/app/(dashboard)/disputa/buscar/components/atoms/title/index.tsx
@@ -1,14 +1,14 @@
-import { StyledTitle } from "./styled";
+import styled from "./styled.module.scss";
 
 type TitleProps = {
     title: string[] | string | undefined,
 }
 const Title = ({ title }:TitleProps) => {
     return (
-        <StyledTitle>
+        <div className={styled.title}>
             <h2>{title}</h2>
             <h3>Clique para selecionar o Esporte</h3>
-        </StyledTitle>
+        </div>
     );
 }
 
