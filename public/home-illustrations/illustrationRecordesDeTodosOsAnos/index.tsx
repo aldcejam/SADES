b14793cdcd500e645626e00aea2c41f798a83b6c
aldcejam <aldcejamj923@gmail.com>
2023-06-30 02:50:33 -0300

    UPDATE: recordes-de-todos-os-anos-illustration

--- public/home-illustrations/illustrationRecordesDeTodosOsAnos/index.tsx
+++ public/home-illustrations/illustrationRecordesDeTodosOsAnos/index.tsx
@@ -1,84 +1,9 @@
-"use client"
-import { styled } from "@mui/material/styles";
-
+import "./styled.scss";
 
 const IllustrationRecordesDestaEdicao = () => {
 
-  const StyledIllustrationRecordesDestaEdicao = styled("div")`
-    position: absolute;
-    z-index: 10;
-    width: 100%;
-    height: 100%;
-    
-    svg {
-      .assign-color{
-        fill: ${(props) => props.theme.palette.primary.main};
-      }
-
-      .cls-1 {
-        fill: #263238;
-      }
-      .cls-15,
-      .cls-18,
-      .cls-2,
-      .cls-22,
-      .cls-23,
-      .cls-5 {
-        fill: #fff;
-      }
-      .cls-11,
-      .cls-2,
-      .cls-4 {
-        opacity: 0.2;
-      }
-      .cls-6 {
-        fill: #dd8c6a;
-      }
-      .cls-7 {
-        fill: #df5753;
-      }
-      .cls-8 {
-        fill: #e58a7b;
-      }
-      .cls-23,
-      .cls-9 {
-        opacity: 0.6;
-      }
-      .cls-10,
-      .cls-15,
-      .cls-17,
-      .cls-20 assign-color {
-        opacity: 0.1;
-      }
-      .cls-12 {
-        opacity: 0.4;
-      }
-      .cls-13 {
-        fill: #2e353a;
-      }
-      .cls-14 {
-        fill: #ffc3bd;
-      }
-      .cls-16 {
-        fill: #ed893e;
-      }
-      .cls-18,
-      .cls-19 {
-        opacity: 0.3;
-      }
-      .cls-21 {
-        fill: #ffb573;
-      }
-      .cls-22 {
-        opacity: 0.5;
-      }
-      .cls-24 {
-        fill: #ff5652;
-      }
-    }
-  `;
   return (
-    <StyledIllustrationRecordesDestaEdicao className="illustration-recordes-desta-edicao">
+    <div className="illustration-recordes-desta-edicao">
       <svg viewBox="0 0 287.23 354.52">
         <g id="Camada_2" data-name="Camada 2">
           <g id="Camada_1-2" data-name="Camada 1">
@@ -741,7 +666,7 @@ const IllustrationRecordesDestaEdicao = () => {
           </g>
         </g>
       </svg>
-    </StyledIllustrationRecordesDestaEdicao>
+    </div>
   );
 };
 
