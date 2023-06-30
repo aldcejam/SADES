9338709d6fa1821a30bc2b75936ca371705b208f
aldcejam <aldcejamj923@gmail.com>
2023-06-30 02:47:06 -0300

    UPDATE: couse-illustration

--- public/home-illustrations/illustrationCourse/index.tsx
+++ public/home-illustrations/illustrationCourse/index.tsx
@@ -1,63 +1,8 @@
-"use client"
-import { styled } from "@mui/material/styles";
+import "./styled.scss"
 
-const IllustrationCourse = () => {
-  const StyledIllustrationCourse = styled("div")`
-    position: absolute;
-    z-index: 10;
-    width: 100%;
-    height: 100%;
-    svg {
-      .assign-color {
-        fill: ${(props) => props.theme.palette.primary.main};
-      }
-      .cls-1 {
-        fill: #263238;
-      }
-      .cls-10,
-      .cls-11,
-      .cls-2,
-      .cls-3,
-      .cls-4,
-      .cls-6,
-      .cls-8,
-      .cls-9 {
-        stroke: #263238;
-      }
-      .cls-2 {
-        stroke-width: 1.08px;
-      }
-      .cls-3,
-      .cls-9 {
-        fill: #fff;
-      }
-      .cls-3,
-      .cls-4,
-      .cls-5,
-      .cls-6,
-      .cls-7 {
-        stroke-miterlimit: 10;
-      }
-      .cls-10,
-      .cls-5,
-      .cls-6,
-      .cls-7,
-      .cls-8 {
-        fill: none;
-      }
-      .cls-5,
-      .cls-7 {
-        stroke: #fff;
-      }
-      .cls-5,
-      .cls-7,
-      .cls-8 {
-        stroke-width: 0.5px;
-      }
-    }
-  `;
+const IllustrationCourse = () => { 
   return (
-    <StyledIllustrationCourse className="illustration-course">
+    <div className="illustration-course">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 345.55 282.35">
         <defs></defs>
         <g id="Camada_2" data-name="Camada 2">
@@ -288,7 +233,7 @@ const IllustrationCourse = () => {
           </g>
         </g>
       </svg>
-    </StyledIllustrationCourse>
+    </div>
   );
 };
 
