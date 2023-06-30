3ed8dde33e77bfc48ff6f0f2b5bd7b5bf98f03b2
aldcejam <aldcejamj923@gmail.com>
2023-06-30 02:48:47 -0300

    UPDATE: jogos-do-seu-curso-illustration

--- public/home-illustrations/illustrationJogosDoSeuCurso/index.tsx
+++ public/home-illustrations/illustrationJogosDoSeuCurso/index.tsx
@@ -1,47 +1,8 @@
-"use client"
-import { styled } from "@mui/material/styles";
-
+import "./styled.scss"
 
 const IllustrationJogosDoSeuCurso = () => {
-  const StyledIllustrationJogosDoSeuCurso = styled("div")`
-    position: absolute;
-    z-index: 10;
-    width: 100%;
-    height: 100%;
-    svg { 
-      .assign-color{
-        fill: ${(props) => props.theme.palette.primary.main};
-      }
-      .cls-1 {
-        fill: #b55b52;
-      }
-      .cls-3,
-      .cls-7 {
-        fill: #fff;
-      }
-      .cls-3 {
-        opacity: 0.5;
-      }
-      .cls-3,
-      .cls-5 {
-        isolation: isolate;
-      }
-      .cls-4 {
-        fill: #263238;
-      }
-      .cls-5 {
-        opacity: 0.2;
-      }
-      .cls-6 {
-        opacity: 0.7;
-      }
-      .cls-8 {
-        fill: #b65b52;
-      }
-    }
-  `;
   return (
-    <StyledIllustrationJogosDoSeuCurso className="illustration-jogos-do-seu-curso">
+    <div className="illustration-jogos-do-seu-curso">
       <svg viewBox="0 0 117.55 279.17">
         <g id="Camada_2" data-name="Camada 2">
           <g id="Camada_1-2" data-name="Camada 1">
@@ -234,7 +195,7 @@ const IllustrationJogosDoSeuCurso = () => {
           </g>
         </g>
       </svg>
-    </StyledIllustrationJogosDoSeuCurso>
+    </div>
   );
 };
 
