842b4176052bfb3b54d547b8fb1521a229dfd8d2
aldcejam <aldcejamj923@gmail.com>
2023-06-30 15:22:45 -0300

    UPDATE STYLE: listSport template

--- src/app/(dashboard)/disputa/buscar/components/template/listSports/index.tsx
+++ src/app/(dashboard)/disputa/buscar/components/template/listSports/index.tsx
@@ -1,7 +1,7 @@
 import Title from "../../atoms/title";
 import SelectDataBySport, { UpdateSportsProps } from "components/organisms/selectDataBySport";
 import { memo } from "react";
-import { StyledListSports } from "./styled"; 
+import styled from "./styled.module.scss"; 
 
 type ListSportsProps = {
     course: any,
@@ -13,14 +13,14 @@ type ListSportsProps = {
 const ListSports = ({ course, ToggleModal, UpdateSport, listSports }: ListSportsProps) => {
 
     return (
-        <StyledListSports>
+        <div className={styled["list-sports"]}>
             <Title title={course ? course : "nenhum curso selecionado"} />
             <SelectDataBySport
                 ToggleModal={ToggleModal}
                 UpdateSport={UpdateSport}
                 listSports={listSports}
             />
-        </StyledListSports>
+        </div>
     )
 }
 export default memo(ListSports);
--- src/app/(dashboard)/disputa/buscar/components/template/listSports/styled.tsx
+++
@@ -1,7 +0,0 @@
-"use client"
-import {styled} from "@mui/material/styles";
-
-export const StyledListSports = styled("div")` 
-    position: relative;
-    z-index: 20;
-`
