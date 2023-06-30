1d1c2460f3e2e534ccbbe5eb6b70953b945ed0f0
aldcejam <aldcejamj923@gmail.com>
2023-06-30 14:16:52 -0300

    STYLE UPDATE: HomePageCards templete

--- src/app/(dashboard)/home/components/template/HomePageCards/index.tsx
+++ src/app/(dashboard)/home/components/template/HomePageCards/index.tsx
@@ -2,30 +2,29 @@ import IllustrationEdicoesDaSemadec from "public/home-illustrations/illustration
 import IllustrationJogosDoSeuCurso from "public/home-illustrations/illustrationJogosDoSeuCurso"
 import IllustrationJogosEsportivos from "public/home-illustrations/illustrationJogosEsportivos"
 import IllustrationPublicacoesSobreSemadec from "public/home-illustrations/illustrationPublicacoesSobreSemadec"
-import IllustrationRecordesDestaEdicao from "public/home-illustrations/illustrationRecordesDeTodosOsAnos"  
-import { StyledHomePageCards } from "./styled"
+import IllustrationRecordesDestaEdicao from "public/home-illustrations/illustrationRecordesDeTodosOsAnos"
 import HomePageCard from '../../molecules/HomePageCards';
 import { Routes } from "@base-project/Routes"
 import IllustrationCourse from "public/home-illustrations/illustrationCourse"
-
+import "./styled.scss"
 
 const HomePageCards = () => {
     return (
-        <StyledHomePageCards className="222">
-            <div className="grid-top">
+        <div className={"home-page-cards"}>
+            <div className={"home-page-cards__grid-top"}>
                 <HomePageCard link='/' title='equipes por curso' illustration={<IllustrationCourse />} />
                 <HomePageCard link='/' title='jogos esportivos' illustration={<IllustrationJogosEsportivos />} />
                 <HomePageCard link={`${Routes.findGame}?curso=informatica`} title='jogos do seu curso' illustration={<IllustrationJogosDoSeuCurso />} />
             </div>
-            <div className="grid-left">
+            <div className={"home-page-cards__grid-left"}>
                 <HomePageCard link='/' title='recordes desta edição' illustration={<IllustrationEdicoesDaSemadec />} />
                 <HomePageCard link='/' title='publicações sobre a semadec' illustration={<IllustrationPublicacoesSobreSemadec />} />
             </div>
-            <div className="grid-right">
+            <div className={"home-page-cards__grid-right"}>
                 <HomePageCard link='/' title='recordes de todos os anos' illustration={<IllustrationRecordesDestaEdicao />} />
                 <HomePageCard link='/' title='Edições da semadec' illustration={<IllustrationRecordesDestaEdicao />} />
             </div>
-        </StyledHomePageCards>
+        </div >
     )
 }
 
