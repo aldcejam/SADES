823fc6d129099fc28e18035d9bf7e18bbd0b384f
aldcejam <aldcejamj923@gmail.com>
2023-10-28 11:42:44 -0300

    tentative: page /disputa/partida

--- src/app/(pages)/(dashboard)/disputa/@modal/(.)partida/[id]/page.tsx
+++ src/app/(pages)/(dashboard)/disputa/@modal/(.)partida/[id]/page.tsx
@@ -1,11 +1,10 @@
 import Modal from '../../../partida/components/template/modal';
 import { PartidaContent } from '../../../partida/components/template/partidaContent';
-import styled from './styled.module.scss';
 
 export default function Page({ params }: any) {
   return (
-    <Modal>
+    <div>
       <PartidaContent display="modal" />
-    </Modal>
+    </div>
   );
 }
